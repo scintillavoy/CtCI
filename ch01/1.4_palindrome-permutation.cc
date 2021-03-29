02c8a7880f4d03be933243a17c1f7ff2caa8aa7c
Moon Dongyoung <scintillavoy@gmail.com>
2021-03-29 18:43:33 +0900

    Solve 1.4

---
+++ ch01/1.4_palindrome-permutation.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+// Time complexity: O(n)
+// Bit vector
+bool PalindromePermutation(string str) {
+    int check = 0;
+    for (const auto &x : str) {
+        if (isalpha(x)) {                               // Check alphabets only
+            check ^= 1 << tolower(x)-'a';
+        }
+    }
+    return check == 0 || (check & (check-1)) == 0;      // Caution !
+}
+
+int main() {
+    cout << PalindromePermutation("Tact Coa") << endl;  // true
+    cout << PalindromePermutation("T1 ct C1a") << endl; // true
+    cout << PalindromePermutation("Tact oA") << endl;   // false
+    cout << PalindromePermutation("aaabbccc") << endl;  // false
+    return 0;
+}
